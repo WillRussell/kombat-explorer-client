e73cf048f418fd7134f83b91de008bf4b014aa59
WillRussell <wrussell35@gmail.com>
2020-10-01 21:54:47 -0700

    Add hello world
    - adds scss config
    - adds roboto import
    - run lint auto-fix across project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.scss';
+
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h6">
+        Hello World
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
