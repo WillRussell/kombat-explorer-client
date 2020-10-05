73ea54042d37d12276046799715293bc63181c01
WillRussell <wrussell35@gmail.com>
2020-10-04 18:52:50 -0700

    Unit test amends

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Router><App /></Router>);
+  const linkElement = getByText(/react material starter/i);
   expect(linkElement).toBeInTheDocument();
 });
