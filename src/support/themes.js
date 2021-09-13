01f36e6aa1b34567cc880a118cbc4d0894895ecb
willrussell <wrussell35@gmail.com>
2021-09-13 15:10:50 -0700

    Amend Material theme switch opts
    - adds error color & misc. color updates

--- src/support/themes.js
+++ src/support/themes.js
@@ -1,13 +1,26 @@
+import {
+  blue, lightBlue, red, teal,
+} from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: teal,
+    secondary: lightBlue,
+    error: {
+      main: red[400],
+    },
   },
 });
 
 export const lightTheme = createMuiTheme({
   palette: {
     type: 'light',
+    primary: teal,
+    secondary: blue,
+    error: {
+      main: red[400],
+    },
   },
 });
